6a7da5b779af2189019c0452fc05a1f5c33b7f87
asokhatch <asokhatch@localhost>
2024-09-17 17:02:06 +0200

    Exo1

---
+++ TPO_Exo1/src/tpo_exo1/TPO_Exo1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tpo_exo1;
+
+import java.util.Scanner;
+
+/**
+ *Sokhatch Arthur
+ * TDC
+ * @author asokhatch
+ */
+public class TPO_Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Bonjour");
+       System.out.println("Au revoir");
+       String prenom ;
+       Scanner sc;
+       sc = new Scanner(System.in); System.out.println("Bonjour, quel est votre prenom ?"); prenom = sc.nextLine();
+    }
+    
+}
