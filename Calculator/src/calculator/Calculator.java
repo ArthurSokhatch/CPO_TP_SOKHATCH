a7b48592ea5076b210434234227e7a0c040af7f1
asokhatch <asokhatch@localhost>
2024-09-22 16:58:19 +0200

    Calculator

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -27,17 +27,56 @@ public class Calculator {
         System.out.println("3) multiply");
         System.out.println("4) divide");
         System.out.println("5) modulo");
-        int operateur = scanner.nextInt();   
+        int operateur = scanner.nextInt();  
+        
+        
+        if (operateur < 1 || operateur > 5) {
+            System.out.println("Error: Invalid operator selected. Please choose a number between 1 and 5.");
+        } else {
         System.out.println("Entrer une première valeur:");
         int operande1 = scanner.nextInt(); 
         System.out.println("Entrer une seconde valeur:");
         int operande2 = scanner.nextInt(); 
         
-           
         
-    
-       
-            
+        int resultat = 0;
+        boolean operationValide = true;
+
+        // Effectuer l'opération en fonction de l'opérateur choisi
+        if (operateur == 1) { // Addition
+            resultat = operande1 + operande2;
+        } else if (operateur == 2) { // Soustraction
+            resultat = operande1 - operande2;
+        } else if (operateur == 3) { // Multiplication
+            resultat = operande1 * operande2;
+        } else if (operateur == 4) { // Division
+            if (operande2 != 0) {
+                resultat = operande1 / operande2;
+            } else {
+                System.out.println("Error: Division by zero is not allowed.");
+                operationValide = false;
+            }
+        } else if (operateur == 5) { // Modulo
+            if (operande2 != 0) {
+                resultat = operande1 % operande2;
+            } else {
+                System.out.println("Error: Division by zero is not allowed.");
+                operationValide = false;
+            }
+        } else {
+            System.out.println("Error: Invalid operator selected.");
+            operationValide = false;
+        }
+
+        // Afficher le résultat si l'opération est valide
+        if (operationValide) {
+            System.out.println("Le resultat est: " + resultat);
+        }
+
+        // Fermer le scanner
+        scanner.close();
+          }        
     }
-    
-}
+        
+}  
+
