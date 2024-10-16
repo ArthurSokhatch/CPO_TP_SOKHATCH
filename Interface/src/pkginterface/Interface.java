d6fea8e17c37ba0fae62d6b1e2b90d18e10b4e7f
asokhatch <asokhatch@localhost>
2024-10-16 12:17:31 +0200

    Interface graphique

---
+++ Interface/src/pkginterface/Interface.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkginterface;
+
+/**
+ *
+ * @author asokhatch
+ */
+public class Interface {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Figure1 f = new Figure1();
+        f.show();
+        // TODO code application logic here
+    }
+    
+}
